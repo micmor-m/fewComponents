ac4286141e2965e835d521988ab8782798768780
mic <micmor.m@gmail.com>
2021-08-24 22:18:44 -0400

    add card component and its style

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 //import styles from "./Card.module.css";
 import "./Card.css";
 
-const Card = (props) => {
-  return <div className="card">{props.children}</div>;
+const Card = ({ className, children }) => {
+	return <div className={`card ${className}`}>{children}</div>;
 };
 
 export default Card;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,27 @@
 import React from "react";
 import Card from "../UI/Card";
+import "./AddUser.css";
 
 const AddUsers = (props) => {
-  function addUserHandler(event) {
-    event.preventDefault();
-  }
+	function addUserHandler(event) {
+		event.preventDefault();
+	}
 
-  return (
-    <Card classMane="input">
-      <form onSubmit={(event) => addUserHandler(event)}>
-        <label htmlFor="name">Username</label>
-        <input type="text" id="username" placeholder="Enter your name" />
-        <label htmlFor="age">Age</label>
-        <input type="number" id="age" placeholder="Enter your age" />
-        <button type="submit">Add User</button>
-      </form>
-    </Card>
-  );
+	return (
+		<Card className="input">
+			<form onSubmit={(event) => addUserHandler(event)}>
+				<label htmlFor="name">Username</label>
+				<input
+					type="text"
+					id="username"
+					placeholder="Enter your name"
+				/>
+				<label htmlFor="age">Age</label>
+				<input type="number" id="age" placeholder="Enter your age" />
+				<button type="submit">Add User</button>
+			</form>
+		</Card>
+	);
 };
 
 export default AddUsers;
